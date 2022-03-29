118b3c163d999d77636355b80e901ae5ae79c1b7
ALONE <102422094+improking@users.noreply.github.com>
2022-03-29 19:24:52 +0530

    Update utils.py

--- ALONE/utils.py
+++ ALONE/utils.py
@@ -1,5 +1,5 @@
-# By < ANKIT KUMAR >
-# // NoUB SPAMMERBOT MADE BY ©LEGEND-ANKIT™ //
+# By < ₳ⱠØ₦Ɇ >
+# // SPAMMERBOT MADE BY ₳ⱠØ₦Ɇ //
 
 import sys
 import logging
@@ -8,11 +8,11 @@ from telethon import TelegramClient, events
 from pathlib import Path
 
 def load_plugins(plugin_name):
-    path = Path(f"ANKIT/plugins/{plugin_name}.py")
-    name = "ANKIT.plugins.{}".format(plugin_name)
+    path = Path(f"ALONE/plugins/{plugin_name}.py")
+    name = "ALONE.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["ANKIT.plugins." + plugin_name] = load
-    print("NoUB SPAMMER BOT has IMPoRTED " + plugin_name)
+    sys.modules["ALONE.plugins." + plugin_name] = load
+    print("₳ⱠØ₦Ɇ SPAMMER BOT has IMPoRTED " + plugin_name)
